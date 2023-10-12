1e67b73534baf46a2a6fb86ccbdedd4a309ef60a
btbatux <rct-batuhan@hotmail.com>
2023-10-13 01:26:06 +0300

    Taksimetre
    Taksimetre ücreti hesplama ornegi

---
+++ Taksimetre/TaksiMetre.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class TaksiMetre {
+    public static void main(String[] args) {
+
+        double toplam, km, kmBasiUcret = 2.20, sabitUcret, minTutar = 20.0, acilisUcreti = 10.0;
+
+        Scanner input = new Scanner(System.in);
+        System.out.println("Gidilen Km'yi Giriniz:");
+
+        km = input.nextInt();
+        toplam = km * kmBasiUcret;
+        toplam += acilisUcreti;
+
+        if (toplam < 20) {
+            toplam = 20;
+            System.out.println("Tutar:" + toplam);
+
+        } else {
+            System.out.println("Tutar: " + toplam);
+        }
+        input.close();
+
+    }
+
+}
