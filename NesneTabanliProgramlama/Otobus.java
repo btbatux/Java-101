2e427b14a92b6de2966e3fcea29a4b0f5c266f8e
btbatux <rct-batuhan@hotmail.com>
2023-10-21 02:53:16 +0300

    Java-Siniflar
    Java-Siniflar+Method

--- NesneTabanliProgramlama/Otobus.java
+++ NesneTabanliProgramlama/Otobus.java
@@ -3,6 +3,7 @@ package NesneTabanliProgramlama;
 public class Otobus {
 
     int kapasite;
+    int mevcutYolcu;
     int fiyat;
     String nereden;
     String nereye;
@@ -11,10 +12,30 @@ public class Otobus {
     public void bilgiAl() {
         System.out.println("Arac Bilgileri: " + bilgi);
         System.out.println("Koltuk Kapasitesi: " + kapasite);
+        System.out.println("Mevut Yolcu: " + mevcutYolcu);
         System.out.println("Fiyat: " + fiyat);
         System.out.println("Nereden: " + nereden);
         System.out.println("Nereye: " + nereye);
 
+    }
+
+    public void yolcuAl(int yolcuMiktarı) {
+        mevcutYolcu += yolcuMiktarı;
+
+        if (mevcutYolcu == kapasite) {
+            System.out.println("Tam Kapasite Dolu ! ");
+        } else
+            System.out.println("Boş yer sayısı : " + (kapasite - mevcutYolcu));
+    }
+
+    public void yolcuIndır(int indirmeMiktarı) {
+        mevcutYolcu -= indirmeMiktarı;
+
+        if (mevcutYolcu == 0) {
+            System.out.println("Otobus tamamen boş");
+        } else {
+            System.out.println("Mevcut Yolcular : " + mevcutYolcu + " Kişi");
+        }
     }
 
 }
--- NesneTabanliProgramlama/OtobusMain.java
+++ NesneTabanliProgramlama/OtobusMain.java
@@ -5,20 +5,27 @@ public class OtobusMain {
     public static void main(String[] args) {
 
         Otobus kamilKoc = new Otobus();
+
         kamilKoc.bilgi = "Kamilkoc";
-        kamilKoc.kapasite = 55;
+        kamilKoc.kapasite = 60;
+        kamilKoc.mevcutYolcu = 20;
         kamilKoc.fiyat = 850;
         kamilKoc.nereden = "Kahramanmaras";
         kamilKoc.nereye = "Istanbul";
 
         Otobus aksu = new Otobus();
+
         aksu.bilgi = "Aksu";
-        aksu.kapasite = 100;
+        aksu.kapasite = 90;
+        aksu.mevcutYolcu = 30;
         aksu.fiyat = 550;
         aksu.nereden = "Sivas";
         aksu.nereye = "Izmır";
 
-        kamilKoc.bilgiAl();
-        aksu.bilgiAl();
+        // kamilKoc.bilgiAl();
+        // aksu.bilgiAl();
+
+        kamilKoc.yolcuAl(40);
+        kamilKoc.yolcuIndır(59);
     }
 }
