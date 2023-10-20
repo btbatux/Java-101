5401d4e1c721dd87902d8fe9ba5bae0bdfaa4e5a
btbatux <rct-batuhan@hotmail.com>
2023-10-20 21:50:52 +0300

    Update DonguVeDiziler.java

--- DonguVeDiziler/DonguVeDiziler.java
+++ DonguVeDiziler/DonguVeDiziler.java
@@ -1,10 +1,12 @@
+import java.util.Arrays;
 import java.util.Scanner;
 
-public class DonguDizler {
+public class DonguVeDiziler {
     public static void main(String[] args) {
 
         try (Scanner input = new Scanner(System.in)) {
             int boyut;
+            int sembol;
 
             System.out.println("Dizinin boyutunu gir : ");
             boyut = input.nextInt();
@@ -18,9 +20,20 @@ public class DonguDizler {
                 dizi[i] = input.nextInt();
 
             }
-            for (int i = 0; i < dizi.length; i++) {
-                System.out.println((i + 1) + ". Sayı " + dizi[i]);
+            System.out.println("Küçükten Büyüğe Sıralamak İçin 1 'e Basınız Yoksa Herhangi Bir Sayıya Basınız : ");
+            sembol = input.nextInt();
+
+            if (sembol == 1) {
+                Arrays.sort(dizi);
+                for (int i = 0; i < dizi.length; i++) {
+                    System.out.println((i + 1) + ". Sayı : " + dizi[i]);
+                }
+            } else if (sembol != 1) {
+                for (int k : dizi) {
+                    System.out.println(k);
+                }
             }
+
         }
 
     }
