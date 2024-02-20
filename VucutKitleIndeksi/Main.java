9184cf1f249c0bfbe17e038091d511b19c9d4802
Batuhan Şatıroğlu <87141556+btbatux@users.noreply.github.com>
2024-02-21 00:30:39 +0300

    Update Main.java

--- VucutKitleIndeksi/Main.java
+++ VucutKitleIndeksi/Main.java
@@ -6,18 +6,18 @@ public class Main {
     public static void main(String[] args) {
 
         Scanner input = new Scanner(System.in);
-        double result, kg, stature;
+        double result, kg, height;
 
         while (true) {
             System.out.println("Boyunuzu (metre cinsinden) giriniz: ");
-            stature = input.nextDouble(); // Boy bilgisini aldık
+            height = input.nextDouble(); // Boy bilgisini aldık
 
             System.out.println("Kilonuzu giriniz: ");
             kg = input.nextDouble(); // Kilo bilgisini aldık
 
-            if (stature > 0 && kg > 0) { // Negatif değer girişini kontrol ettik
+            if (height > 0 && kg > 0) { // Negatif değer girişini kontrol ettik
 
-                result = kg / (stature * stature); // Vucüt kitle indeksini hesapla
+                result = kg / (height * height); // Vucüt kitle indeksini hesapla
                 System.out.println("Vücud kitle indeksiniz: " + result);
                 // Doğru giriş yapıldıysa, döngüyü kırılır
                 break;
