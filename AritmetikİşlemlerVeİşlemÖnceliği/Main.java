ec8d5a63e72312b3c6d8eec7abc8e4a411eac4b7
Batuhan Şatıroğlu <87141556+btbatux@users.noreply.github.com>
2024-02-20 23:58:13 +0300

    Update Main.java

--- AritmetikİşlemlerVeİşlemÖnceliği/Main.java
+++ AritmetikİşlemlerVeİşlemÖnceliği/Main.java
@@ -4,17 +4,17 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        int a, b, c, result;
+        int a, b, c, result; // Değişkenler
 
         Scanner input = new Scanner(System.in);
         System.out.print("Birinci sayıyı giriniz:");
-        a = input.nextInt();
+        a = input.nextInt(); // a sayısı kullanıcıdan alında
         System.out.print("İkinci sayıyı giriniz:");
-        b = input.nextInt();
+        b = input.nextInt(); // b sayısı kullanıcıdan alında
         System.out.print("Üçüncü sayıyı giriniz:");
-        c = input.nextInt();
+        c = input.nextInt(); // c sayısı kullanıcıdan alında
 
-        result = a + b * c - b;
+        result = a + b * c - b; // işlem önceliğinegöre hesaplandı
 
         System.out.println("Sonuç: " + result);
 
