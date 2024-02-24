52b72279c95ebaf94a2ba3002b0f10c5a5a81962
Batuhan Şatıroğlu <87141556+btbatux@users.noreply.github.com>
2024-02-25 01:50:27 +0300

    Update Main.java

--- KucuktenBuyugeSiralama/Main.java
+++ KucuktenBuyugeSiralama/Main.java
@@ -4,31 +4,37 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
+        Scanner scanner = new Scanner(System.in); // Kullanıcıdan girdi almak için Scanner nesnesi oluşturuluyor
 
+        // Kullanıcıdan üç sayı girmesini iste.
         System.out.println("Lütfen üç sayı giriniz:");
-        int sayi1 = scanner.nextInt();
-        int sayi2 = scanner.nextInt();
-        int sayi3 = scanner.nextInt();
+        int sayi1 = scanner.nextInt(); // İlk sayıyı oku.
+        int sayi2 = scanner.nextInt(); // İkinci sayıyı oku.
+        int sayi3 = scanner.nextInt(); // Üçüncü sayıyı oku.
 
-        if (sayi1 > sayi2) { // ilk sa
-            int temp = sayi1;
-            sayi1 = sayi2;
-            sayi2 = temp;
-        }
-
-        if (sayi2 > sayi3) {
-            int temp = sayi2;
-            sayi2 = sayi3;
-            sayi3 = temp;
-        }
-
+        // sayi1 ve sayi2'yi karşılaştır ve gerekirse yerlerini değiştir
         if (sayi1 > sayi2) {
             int temp = sayi1;
             sayi1 = sayi2;
             sayi2 = temp;
         }
 
+        // sayi2 ve sayi3'ü karşılaştır ve gerekirse yerlerini değiştir
+        if (sayi2 > sayi3) {
+            int temp = sayi2;
+            sayi2 = sayi3;
+            sayi3 = temp;
+        }
+
+        // Yer değiştirme işlemleri sonrasında sayi1 ve sayi2'nin sıralamasını tekrar
+        // kontrol et
+        if (sayi1 > sayi2) {
+            int temp = sayi1;
+            sayi1 = sayi2;
+            sayi2 = temp;
+        }
+
+        // Sıralanmış sayıları ekrana yazdır.
         System.out.println("Sayılar küçükten büyüğe sıralanmıştır: " + sayi1 + ", " + sayi2 + ", " + sayi3);
     }
 }
