2f02cf4c50689e8c49e219680ade2d4f2313000b
Batuhan Şatıroğlu <87141556+btbatux@users.noreply.github.com>
2024-02-25 23:16:28 +0300

    Update Main.java

--- CinZodyağıHesaplama/Main.java
+++ CinZodyağıHesaplama/Main.java
@@ -1,9 +1,61 @@
 package CinZodyağıHesaplama;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
 
-        System.out.println("Çin Zodyağı ");
+        //Çin zodyağı hesaplanırken kişinin doğum yılının 12 ile bölümünde kalana göre bulunur.
+       Scanner scan = new Scanner(System.in);
+		System.out.println("Doğum yılınızı giriniz: ");
+        
+		int dogumTarihi = scan.nextInt();
+		int sonuc = dogumTarihi % 12;
+		
+		switch(sonuc)
+		{
+		case 0:   // dogumTarihi % 12 = 0  
+			System.out.println("Çin Zodyağı: Maymun");
+			break;
+
+		case 1:  // dogumTarihi % 12 = 1 
+			System.out.println("Çin Zodyağı: Horoz");
+			break;
+            
+		case 2: // dogumTarihi % 12 = 2 
+			System.out.println("Çin Zodyağı: Köpek");
+			break;	
+		case 3: // dogumTarihi % 12 = 3 
+			System.out.println("Çin Zodyağı: Domuz");
+			break;
+		case 4: // dogumTarihi % 12 = 4 
+			System.out.println("Çin Zodyağı: Fare");
+			break;
+		case 5: // dogumTarihi % 12 = 5 
+			System.out.println("Çin Zodyağı: Öküz");
+			break;
+		case 6: // dogumTarihi % 12 = 6 
+			System.out.println("Çin Zodyağı: Kaplan");
+			break;
+		case 7: // dogumTarihi % 12 = 7 
+			System.out.println("Çin Zodyağı: Tavşan");
+			break;
+		case 8: // dogumTarihi % 12 = 8 
+			System.out.println("Çin Zodyağı: Ejderha");
+			break;
+		case 9: // dogumTarihi % 12 = 9 
+			System.out.println("Çin Zodyağı: Yılan");
+			break;	
+		case 10: // dogumTarihi % 12 = 10 
+			System.out.println("Çin Zodyağı: At");
+			break;	
+		case 11: // dogumTarihi % 12 = 11 
+			System.out.println("Çin Zodyağı: Koyun");
+			break;	
+		default:  
+			System.out.println("Yanlış değer girdiniz!");
+			break;	
+		}
 
     }
 }
