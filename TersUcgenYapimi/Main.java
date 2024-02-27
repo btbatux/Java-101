ac961d3721334fd101074b9be048bff30c50e4e8
Batuhan Şatıroğlu <87141556+btbatux@users.noreply.github.com>
2024-02-28 01:56:48 +0300

    .
    .

--- TersUcgenYapimi/Main.java
+++ TersUcgenYapimi/Main.java
@@ -12,9 +12,9 @@ public class Main {
         System.out.print("Ters üçgen basamak sayısını girin: ");
         basamakSayisi = input.nextInt();
 
-        for (int i = 0; i < basamakSayisi; i++) {
+        for (int i = 0; i < basamakSayisi; i++) { // Basamakları döner
             // Print stars
-            for (int j = 0; j < (basamakSayisi - i) * 2 - 1; j++) {
+            for (int j = 0; j < (basamakSayisi - i) * 2 - 1; j++) { // Her bir basamak için yıldızları yazar.
                 System.out.print("*");
             }
             System.out.println();
