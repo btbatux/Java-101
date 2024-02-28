de19ecd711f7b9f50695278a8c89c0eb57353767
Batuhan Şatıroğlu <87141556+btbatux@users.noreply.github.com>
2024-02-28 15:00:23 +0300

    Update Main.java

--- FibonacciSerisi/Main.java
+++ FibonacciSerisi/Main.java
@@ -1,14 +1,19 @@
 package FibonacciSerisi;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
 
-        int maxNumber = 10, // 10 serili fibonacci
+        Scanner input = new Scanner(System.in);
+        int maxNumber, // Fibonacci serisi
                 previousNumber = 0, // Başlangıç değeri
                 nextNumber = 1; // Bir sonraki değer
 
-        System.out.print("Fibonacci Series of " + maxNumber + " numbers:");
+        System.out.print(" Fibonacci Serisini Giriniz: ");
+        maxNumber = input.nextInt();
+        System.out.print(" Fibonacci Series of " + maxNumber + " numbers: ");
 
         int i = 1; // Döngü başlangıç değeri
         while (i <= maxNumber) {
