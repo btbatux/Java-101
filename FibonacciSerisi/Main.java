02ea07dd14a9460267f82a6c794db35c6672440b
Batuhan Şatıroğlu <87141556+btbatux@users.noreply.github.com>
2024-02-28 14:49:14 +0300

    Update Main.java

--- FibonacciSerisi/Main.java
+++ FibonacciSerisi/Main.java
@@ -4,10 +4,13 @@ public class Main {
 
     public static void main(String[] args) {
 
-        int maxNumber = 10, previousNumber = 0, nextNumber = 1;
+        int maxNumber = 10, // 10 serili fibonacci
+                previousNumber = 0, // Başlangıç değeri
+                nextNumber = 1; // Bir sonraki değer
+
         System.out.print("Fibonacci Series of " + maxNumber + " numbers:");
 
-        int i = 1;
+        int i = 1; // Döngü başlangıç değeri
         while (i <= maxNumber) {
             System.out.print(previousNumber + " ");
             int sum = previousNumber + nextNumber;
