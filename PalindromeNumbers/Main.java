1e860227c0d43dbdc7ea7ec3992f14c56ac153a4
Batuhan Şatıroğlu <87141556+btbatux@users.noreply.github.com>
2024-02-29 17:17:16 +0300

    Update Main.java

--- PalindromeNumbers/Main.java
+++ PalindromeNumbers/Main.java
@@ -19,7 +19,7 @@ public class Main {
     }
 
     public static void main(String[] args) {
-        // 100 ile 200 arasındaki palindrom sayıları bulup yazdırıyoruz
+        // palindrom sayıları bulup yazdırıyoruz
         System.out.println(isPalindrome(11));
     }
 }
