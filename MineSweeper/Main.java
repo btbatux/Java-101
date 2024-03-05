0415742e3d157b6dc3044f6dd589491d93b27f5b
Batuhan Şatıroğlu <87141556+btbatux@users.noreply.github.com>
2024-03-06 02:53:37 +0300

    comment update
    comment update

--- MineSweeper/Main.java
+++ MineSweeper/Main.java
@@ -4,6 +4,7 @@ public class Main {
     public static void main(String[] args) {
 
         MineSweeper mine = new MineSweeper(); // MineSweeper sınıfından nesne oluşturduk
+
         mine.run(); // mine nesnesinini run metodunu çağırdık ve oyunu başlattık.
 
     }
